469283de55fe31d6f6097e9a862ca2d518f7e6f3
yordhis <yordhis.10@gmail.com>
2024-04-12 17:09:32 -0400

    v1 restuarante

---
+++ README.md
@@ -0,0 +1,6 @@
+# botanero-ventas
+ Sistema para un restaurante, local donde vendan comida, botanas, etc.
+
+ # NODE 16.20.2
+ # npm 8.19.4
+ # php 8.1.10
---
+++ src/Servicios/HttpService.js
@@ -0,0 +1,42 @@
+// const RUTA_GLOBAL = "http://localhost/botanero-ventas/api/"
+
+const RUTA_GLOBAL = `${window.location.protocol}//${window.location.host}/api/`;
+
+const HttpService = {
+    async registrar(datos, ruta){
+        const respuesta = await fetch(RUTA_GLOBAL + ruta, {
+            method: "post",
+            body: JSON.stringify(datos),
+        });
+        let resultado = await respuesta.json()
+        return resultado
+    },
+
+    async obtenerConDatos(datos, ruta){
+        const respuesta = await fetch(RUTA_GLOBAL + ruta, {
+            method: "post",
+            body: JSON.stringify(datos),
+        });
+        let resultado = await respuesta.json()
+        return resultado
+    },
+
+
+    async obtener(ruta){
+        let respuesta = await fetch(RUTA_GLOBAL + ruta)
+        let datos = await respuesta.json()
+        return datos
+    },
+
+    async eliminar(ruta, id) {
+        const respuesta = await fetch(RUTA_GLOBAL + ruta, {
+            method: "post",
+            body: JSON.stringify(id),
+        });
+        let resultado = await respuesta.json()
+        return resultado
+    }
+
+}
+
+export default  HttpService 
